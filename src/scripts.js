9aee7bb1510fc049a9889b3638f844d4516de671
Ravindra M Suthar <ravindra.suthar@me.com>
2022-06-16 14:36:03 +0530

    initial commit

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,5 @@
 function findActiveEl(el) {
-  var El = document.querySelector(el);
+  const El = document.querySelector(el);
 
   const actEl =
     El.querySelector(".active") &&
@@ -8,7 +8,37 @@ function findActiveEl(el) {
     El.style.background =
       "linear-gradient(#000, #000) left/" +
       actEl.x +
-      "px 1px no-repeat, linear-gradient(#ccc, #ccc) left/100% 1px no-repeat";
+      "px 1px no-repeat, linear-gradient(#c8c8c8, #c8c8c8) left/100% 1px no-repeat";
 }
 
 findActiveEl(".progress");
+
+function isFst(){
+  const El = document.querySelector(".progress li.active");
+  return !(El.previousElementSibling);
+}
+function isLst(){
+  const El = document.querySelector(".progress li.active");
+  return !(El.nextElementSibling || El.classList.contains('end'));
+}
+ 
+document.getElementById("nxt").addEventListener("click",nextNav,true)
+document.getElementById("prev").addEventListener("click",prevNav,true)
+function nextNav(){
+  const El = document.querySelector(".progress li.active");
+  if(!isLst()){
+    El.nextElementSibling.classList.add("active");
+    El.classList.remove("active");    
+  }else{
+    El.classList.add("completed");  
+  }
+}
+function prevNav(){
+  const El = document.querySelector(".progress li.active");
+  if(!isFst()){
+    El.previousElementSibling.classList.add("active");
+    El.classList.remove("active");
+    El.classList.remove("completed");
+  }  
+}
+
